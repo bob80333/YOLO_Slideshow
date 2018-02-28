0039da2bfa70cf15604813bb2db1ee1ee59d8f2c
Eric Engelhart <bob80333@gmail.com>
2018-02-27 21:27:50 -0500

    Initial Commit

---
+++ image_handler.py
@@ -0,0 +1,79 @@
+# Eric Engelhart
+# step 1 download images to dir
+# step 2 run the thing against them
+# step 3 put the output into another folder
+# step 4 remove all input/output images
+# step 5 repeat after 5 seconds
+
+import subprocess
+import time
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+YOLO_COMMAND = "flow --model cfg/yolo.cfg --load bin/yolo.weights --imgDir websiteImages/ --threshold 0.3"
+MOVE_COMMAND = "mv -v websiteImages/out/* ../slideshow/"
+REMOVE_INPUT_COMMAND = "rm -f websiteImages/*"
+REMOVE_OUTPUT_COMMAND = "rm -f websiteImages/out/*"
+WEBSITE_LINK = "http://mechrono.x10host.com/vult/img/"
+WEBSITE_IMAGE_DIRECTORY = "websiteImages/"
+
+already_seen = set([])
+
+
+# download the images, and make sure they haven't been downloaded before
+def download_images():
+    with urllib.request.urlopen(WEBSITE_LINK) as response:
+        html = response.read()
+        soup = BeautifulSoup(html, "lxml")
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if "/" in href or href in already_seen:
+                continue
+            else:
+                already_seen.add(href)
+                urllib.request.urlretrieve(WEBSITE_LINK + href, WEBSITE_IMAGE_DIRECTORY + href)
+
+
+
+
+def run_yolo():
+    # run yolo command
+    yolo = subprocess.Popen(YOLO_COMMAND)
+    # wait for yolo to finish processing
+    yolo.wait()
+
+
+def move_output():
+    # run move command
+    move = subprocess.Popen(MOVE_COMMAND)
+    # wait for files to be moved
+    move.wait()
+
+
+def cleanup():
+    # run remove commands
+    rm_input = subprocess.Popen(REMOVE_INPUT_COMMAND)
+    rm_output = subprocess.Popen(REMOVE_OUTPUT_COMMAND)
+
+    # wait for both commands to complete
+    [rm.wait() for rm in (rm_input, rm_output)]
+
+
+def main():
+    while True:
+        # wait between steps for a half a second just in case
+        download_images()
+        time.sleep(.5)
+        run_yolo()
+        time.sleep(.5)
+        move_output()
+        time.sleep(.5)
+        cleanup()
+        # wait a few seconds before re-running
+        time.sleep(2.5)
+
+# run the program
+#main()
+
+download_images()
